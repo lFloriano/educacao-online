a6a3a31305ad2aba42fd3e7a6289f03d7a085865
= <=>
2025-12-19 18:51:14 -0300

    Aumento na cobertura de testes de cursos

--- Src/IntegrationTests/EducacaoOnline.IntegrationTests/CursosControllerTests.cs
+++ Src/IntegrationTests/EducacaoOnline.IntegrationTests/CursosControllerTests.cs
@@ -87,5 +87,94 @@ namespace EducacaoOnline.IntegrationTests
             aula.Titulo.Should().Be("Escritos Aristotélicos");
             #endregion
         }
+
+        [Fact]
+        public async Task ConsultaDeCursoPorIdBemSucedida()
+        {
+            #region cadastro de curso
+
+            var cursoVm = new CadastrarCursoVm()
+            {
+                Nome = "Introdução à Filosofia",
+                Descricao = "Curso rápido de filosofia",
+                Valor = 50,
+                MaterialDidatico = "Apostilas disponiveis em: https://www.curso-filosofia.com.br/material",
+                NumeroAulas = 2
+            };
+
+            var cursoResponse = await _httpClient.PostAsJsonAsync("/api/cursos", cursoVm);
+            var curso = await cursoResponse.Content.ReadFromJsonAsync<CursoDto>();
+            cursoResponse.EnsureSuccessStatusCode();
+
+            #endregion
+
+            #region consulta curso criado
+
+            var consultaResponse = await _httpClient.GetAsync($"/api/cursos/{curso.Id}");
+            var cursoConsultado = await consultaResponse.Content.ReadFromJsonAsync<CursoDto>();
+
+
+            cursoConsultado.Should().NotBeNull();
+            cursoConsultado.Id.Should().Be(curso.Id);
+            cursoConsultado.Nome.Should().Be(curso.Nome);
+            #endregion
+
+        }
+
+        [Fact]
+        public async Task ConsultaDeAulasPorCursoIdBemSucedida()
+        {
+            #region cadastro de curso
+
+            var cursoVm = new CadastrarCursoVm()
+            {
+                Nome = "Introdução à Filosofia",
+                Descricao = "Curso rápido de filosofia",
+                Valor = 50,
+                MaterialDidatico = "Apostilas disponiveis em: https://www.curso-filosofia.com.br/material",
+                NumeroAulas = 2
+            };
+
+            var cursoResponse = await _httpClient.PostAsJsonAsync("/api/cursos", cursoVm);
+            var curso = await cursoResponse.Content.ReadFromJsonAsync<CursoDto>();
+            cursoResponse.EnsureSuccessStatusCode();
+
+            #endregion
+
+            #region consulta curso criado
+
+            var consultaResponse = await _httpClient.GetAsync($"/api/cursos/{curso.Id}");
+            var cursoConsultado = await consultaResponse.Content.ReadFromJsonAsync<CursoDto>();
+
+
+            cursoConsultado.Should().NotBeNull();
+            cursoConsultado.Id.Should().Be(curso.Id);
+            cursoConsultado.Nome.Should().Be(curso.Nome);
+            #endregion
+
+            #region cadastro da aula
+            var aulaVm = new CadastrarAulaVm()
+            {
+                CursoId = curso.Id,
+                Titulo = "Escritos Aristotélicos"
+            };
+
+            var aulaResponse = await _httpClient.PostAsJsonAsync($"api/cursos/{curso.Id.ToString()}/aulas", aulaVm);
+            var aula = await aulaResponse.Content.ReadFromJsonAsync<AulaDto>();
+
+            aula.Should().NotBeNull();
+            aula.Id.Should().NotBeEmpty();
+            aula.Titulo.Should().Be("Escritos Aristotélicos");
+            #endregion
+
+            #region consulta curso criado
+
+            var consultaAulasResponse = await _httpClient.GetAsync($"/api/cursos/{curso.Id}/aulas");
+            var aulasConsultadas = await consultaAulasResponse.Content.ReadFromJsonAsync<IEnumerable<AulaDto>>();
+
+            aulasConsultadas.Should().NotBeNull();
+            consultaAulasResponse.EnsureSuccessStatusCode();
+            #endregion
+        }
     }
 }
